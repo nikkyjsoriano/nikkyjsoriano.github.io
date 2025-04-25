f22ef05287aae7315cb513d1f90d2aea569ecba3
Nikky Soriano <nikkyjsoriano@gmail.com>
2025-04-25 16:11:06 -0400

    Portfolio Landing Page
    - typewriter landing page with ai generated profile asset
    - Uses daisyui and react and includes basic navigation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Landing from "./components/Landing";
+function App() {
+  return (
+    <div className="w-full min-h-screen">
+      <NavBar />
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,64 @@
+function NavBar() {
+  return (
+    <div className="navbar bg-base-100 shadow-sm w-full fixed top-0 left-0 right-0 z-50">
+      <div className="flex-1">
+        <a className="btn btn-ghost lg:text-3xl text-2xl">Nikky Soriano</a>
+      </div>
+      <div className="flex-none hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-xl text-bold">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Projects</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className="flex-none lg:hidden">
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Projects</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
