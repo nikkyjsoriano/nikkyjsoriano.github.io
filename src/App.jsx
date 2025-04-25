24600b70096e9367c8b99c3ece2d9f9b2af5e3e0
Nikky Soriano <nikkyjsoriano@gmail.com>
2025-04-25 16:49:02 -0400

    Language and Tool Sections
    - Added sections for relevant programming languages and skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Landing from "./components/Landing";
+import LanguageSkills from "./components/LanguageSkills";
+
 function App() {
   return (
     <div className="w-full min-h-screen">
       <NavBar />
       <Landing />
+      <LanguageSkills />
     </div>
   );
 }
